1ecbb49b8967ff81e133407392a32acc1027000f
Camden Wagner <camden.i.wagner@ivarcode.net>
2016-11-17 19:52:07 -0500

    initial file setup (index.php, style.css)

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!-- 
+	Website code for skinned game project
+	Currently a work-in-progress
+	Until there is some real work done, there will be very limited documentation. It will come though.
+
+	index.php
+	Camden I. Wagner
+	11/17/2016
+-->
+
+<html>
+<head>
+	<title>skinned</title>
+	<link rel="stylesheet" href="style.css">
+	
+	<script type="text/javascript">
+
+		
+
+		window.addEventListener('load', setup, false);
+
+	</script>
+</head>
+<body>
+
+	<canvas id="game">canvas</canvas>
+
+</body>
+</html>
